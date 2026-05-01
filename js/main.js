1c68bf004ce53d8ced6f7820e8840ff28623dadc
fuseann28 <fuseann28@gmail.com>
2026-04-30 22:06:17 -0500

    Basically everything because i forgot to submit the other day :(

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// JS scripts placed here
+let index = 0;
+showSlide(index);
+
+function showSlide(i) {
+  const slides = document.querySelectorAll(".slide");
+
+  if (i >= slides.length) index = 0;
+  if (i < 0) index = slides.length - 1;
+
+  slides.forEach(slide => slide.style.display = "none");
+  slides[index].style.display = "block";
+}
+
+document.getElementById("next").onclick = () => {
+  index++;
+  showSlide(index);
+};
+
+document.getElementById("prev").onclick = () => {
+  index--;
+  showSlide(index);
+};
